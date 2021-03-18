2f7f1e04ad9212ba2870f7d1093bd8c48e6f3edc
Mano Sriram <mano.sriram0@gmail.com>
2021-03-18 19:24:48 +0530

    Add Rate limiting

--- index.js
+++ index.js
@@ -3,21 +3,29 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const bodyParser = require("body-parser");
 const mysql = require("mysql");
+const rateLimit = require("express-rate-limit");
 
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 15 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+});
+
+app.use(limiter);
 app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + "/views"));
+
 var connection = mysql.createConnection({
     host: "127.0.0.1",
     port: 3306,
-    user:"mysql",
+    user: "mysql",
     user: "root",
     password: "password",
     database: "videorequest"
 });
 connection.connect((err, d) => {
-	if (err) console.log(err);
+    if (err) console.log(err);
 });
 
 app.post("/request", (req, res) => {
